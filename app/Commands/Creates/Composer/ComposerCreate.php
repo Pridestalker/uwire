06ee6930b0f514109725d1a474bd696db2849df7
Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>
2020-01-02 09:52:05 +0100

    updated to laravel-zero 6.0
    Signed-off-by: Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>

--- app/Commands/Creates/Composer/ComposerCreate.php
+++ app/Commands/Creates/Composer/ComposerCreate.php
@@ -21,27 +21,27 @@ class ComposerCreate extends Command
      * @var string
      */
     protected $description = 'Creates composer package.json';
-    
+
     /**
      * @var array[]
      */
     private $requiredPackages;
-    
+
     /**
      * @var array[]
      */
     private $devPackages;
-    
+
     /**
      * @var FileSystemController $fsc
      */
     protected $fsc;
-    
+
     /**
      * @var \Mustache_Engine $engine
      */
     protected $engine;
-    
+
     /**
      * PackageCreate constructor.
      *
@@ -57,7 +57,7 @@ class ComposerCreate extends Command
         $this->fsc = $fileSystemController;
         $this->engine = $engine;
     }
-    
+
     /**
      * Execute the console command.
      *
@@ -66,70 +66,70 @@ class ComposerCreate extends Command
     public function handle()
     {
         $this->setBasePackages();
-        
+
         $this->writeFile();
     }
-    
+
     private function writeFile() {
         $templateDirectory = $this->fsc->getDirectory([
             'resources',
             'templates',
             'json'
         ]);
-        
+
         $template = $templateDirectory . 'composer.json.mustache';
-    
+
         try {
             $package ['contents'] = $this->fsc->fs->get($template);
         } catch (\Exception $e) {
             $this->error($e->getMessage());
             return;
         }
-        
+
         $required = $this->getRequiredPackages();
         $required[count($required) -1]['hasComma'] = false;
-        
+
         $dev = $this->getDevPackages();
         $dev[count($dev) -1]['hasComma'] = false;
-        
+
         $package ['data'] = $this->engine->render($package['contents'], [
             'requiredPackages'  => $required,
             'devPackages'  => $dev
         ]);
-    
+
         return $this->fsc->storeFile(
             $this->fsc->trailingSlashIt($this->fsc->getCurrentDirectory()) . 'composer.json',
             $package['data']
         ) > 0?: false;
     }
-    
+
     private function setBasePackages(): void {
         $packages ['required'] = [
             'yahnis-elsts/plugin-update-checker' => '^4.6',
             'aristath/kirki'                     => '^3.0',
-            'timber/timber'                      => '^1.10',
+            'timber/timber'                      => '^1.13',
             'jjgrainger/posttypes'               => '^2.0',
             'htmlburger/carbon-fields'           => '^3.1',
             'dusank/knapsack'                    => '^10.0'
         ];
-        
+
         $packages ['dev'] = [
             'roave/security-advisories' => 'dev-master'
         ];
-        
+
         foreach ($packages['required'] as $key => $value) {
             $this->addRequiredPackages($key, $value);
         }
-        
+
         foreach ($packages['dev'] as $key => $value) {
             $this->addDevPackages($key, $value);
         }
     }
-    
+
     public function getRequiredPackages(): array {
         return $this->requiredPackages;
     }
-    
+
     public function addRequiredPackages($package, $version = '*'): void {
         $this->requiredPackages [] = [
             'package'   => $package,
@@ -137,11 +137,11 @@ class ComposerCreate extends Command
             'hasComma'  => true,
         ];
     }
-    
+
     public function getDevPackages(): array {
         return $this->devPackages;
     }
-    
+
     public function addDevPackages($package, $version = '*'): void {
         $this->devPackages [] = [
             'package'   => $package,
